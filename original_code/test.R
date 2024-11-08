869ac2b1c076839f9d8eb72506a7db04eafb0e9a
wiggly1993 <benjablonski@hotmail.com>
2024-11-08 09:00:17 +0100

    moved everythig one folder up

---
+++ original_code/test.R
@@ -0,0 +1,9 @@
+tryCatch({
+    library(HyRiM)
+    library(Rglpk)
+    print("Packages loaded successfully")
+}, error = function(e) {
+    print(paste("Error:", e$message))
+})
+
+# print("hello world")

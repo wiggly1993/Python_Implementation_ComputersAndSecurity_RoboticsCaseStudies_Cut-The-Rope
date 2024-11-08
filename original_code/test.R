00c8dc8a5ba07eba9dab2435bba639461ae2f8cd
wiggly1993 <benjablonski@hotmail.com>
2024-11-08 10:17:06 +0100

    ran some small plot in r to see whether it works

--- original_code/test.R
+++ original_code/test.R
@@ -1,9 +1,27 @@
-tryCatch({
-    library(HyRiM)
-    library(Rglpk)
-    print("Packages loaded successfully")
-}, error = function(e) {
-    print(paste("Error:", e$message))
-})
+# tryCatch({
+#     library(HyRiM)
+#     library(Rglpk)
+#     print("Packages loaded successfully")
+# }, error = function(e) {
+#     print(paste("Error:", e$message))
+# })
 
 # print("hello world")
+
+
+# Create a vector of numbers
+x <- c(1, 2, 3, 4, 5)
+y <- x^2
+
+# Print some basic statistics
+print("Basic Statistics:")
+print(paste("Mean of x:", mean(x)))
+print(paste("Sum of x:", sum(x)))
+
+# Create a simple plot
+plot(x, y, 
+     type = "o",
+     col = "blue",
+     main = "Simple Test Plot",
+     xlab = "X values",
+     ylab = "Y values (X squared)")

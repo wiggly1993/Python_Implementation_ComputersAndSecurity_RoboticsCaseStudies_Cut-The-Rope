bdebf2fb7c5beb9e0d4f661cbf963088529f5903
wiggly1993 <benjablonski@hotmail.com>
2024-11-10 13:17:59 +0100

    First attempts to code

--- original_code/experiment_1.R
+++ original_code/experiment_1.R
@@ -5,21 +5,26 @@
 # - defender: periodic 
 # - attack graph: MARA
 
-rm(list = ls())  # clean up the workspace to avoid side-effects
+# Similar to restarting a kernel in python
+rm(list = ls())  
 
+# This part makes sure that everything we will print later on will not go
+# into the console but instead go directly into the .log file
 outputLogFile <- "experiment_1.log"
-
 con <- file(outputLogFile)
 sink(con, append=TRUE)
 sink(con, append=TRUE, type="message")
+
+# Thus this will be printed directly into the log file.
 print(outputLogFile)
 print(date())
 
+# similar to import attack_graph.MARA.R
 source(file = "attack_graph_MARA.R")
 
 attackRateList <- 2  # parameter lambda
 
-# random steps determined from attack rate using a Poisson distribution
+# defines a new function called poisson
 randomSteps <- function(route, attackRate = NULL, defenseRate = NULL) {
   # the value of "attackRate" comes from an external loop
   pdfD <- dpois(x=0:(length(route)-1), lambda = attackRate)
--- original_code/test.R
+++ original_code/test.R
@@ -1,3 +1,8 @@
+z <- c(10, 5, 3)
+print(z)
+
+
+
 # tryCatch({
 #     library(HyRiM)
 #     library(Rglpk)
@@ -7,21 +12,3 @@
 # })
 
 # print("hello world")
-
-
-# Create a vector of numbers
-x <- c(1, 2, 3, 4, 5)
-y <- x^2
-
-# Print some basic statistics
-print("Basic Statistics:")
-print(paste("Mean of x:", mean(x)))
-print(paste("Sum of x:", sum(x)))
-
-# Create a simple plot
-plot(x, y, 
-     type = "o",
-     col = "blue",
-     main = "Simple Test Plot",
-     xlab = "X values",
-     ylab = "Y values (X squared)")
